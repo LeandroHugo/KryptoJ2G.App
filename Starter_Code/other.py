6911fbd42ed788cea75bbd78866802c48906acb0
leandrohugo <125203869+LeandroHugo@users.noreply.github.com>
2023-07-13 11:32:37 -0700

    KJ2GP9C

--- Starter_Code/other.py
+++ Starter_Code/other.py
@@ -1,72 +1,64 @@
+# Here is the modified code:
+
 # Imports
 import os
-import requests
 from dotenv import load_dotenv
-from bip44 import Wallet
-from web3 import Account
-from web3 import middleware
-from web3.gas_strategies.time_based import medium_gas_price_strategy
 import streamlit as st
+from crypto_wallet import generate_account, get_balance, send_transaction
+from web3 import Web3
 
 # Load environment variables
 load_dotenv()
 
-# Wallet functionality
-def generate_account():
-    """Create a digital wallet and Ethereum account from a mnemonic seed phrase."""
-    # Fetch mnemonic from environment variable.
-    mnemonic = os.getenv("MNEMONIC")
+# Initialize a Web3 instance
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
 
-    # Create Wallet Object
-    wallet = Wallet(mnemonic)
+# Database of KryptoJobs2Go candidates including their name, digital address, rating and hourly cost per Ether.
+candidate_database = {
+    "Lane": [
+        "Lane",
+        "0xaC8eB8B2ed5C4a0fC41a84Ee4950F417f67029F0",
+        "4.3",
+        0.20,
+        "Images/lane.jpeg",
+    ],
+    "Ash": [
+        "Ash",
+        "0x2422858F9C4480c2724A309D58Ffd7Ac8bF65396",
+        "5.0",
+        0.33,
+        "Images/ash.jpeg",
+    ],
+    "Jo": [
+        "Jo",
+        "0x8fD00f170FDf3772C5ebdCD90bF257316c69BA45",
+        "4.7",
+        0.19,
+        "Images/jo.jpeg",
+    ],
+    "Kendall": [
+        "Kendall",
+        "0x8fD00f170FDf3772C5ebdCD90bF257316c69BA45",
+        "4.1",
+        0.16,
+        "Images/kendall.jpeg",
+    ],
+}
 
-    # Derive Ethereum Private Key
-    private, public = wallet.derive_account("eth")
+# A list of the KryptoJobs2Go candidates first names
+people = ["Lane", "Ash", "Jo", "Kendall"]
 
-    # Convert private key into an Ethereum account
-    account = Account.privateKeyToAccount(private)
+def get_people():
+    """Display the database of KryptoJobs2Go candidate information."""
+    db_list = list(candidate_database.values())
 
-    return account
-
-def get_balance(w3, address):
-    """Using an Ethereum account address access the balance of Ether"""
-    # Get balance of address in Wei
-    wei_balance = w3.eth.get_balance(address)
-
-    # Convert Wei value to ether
-    ether = w3.fromWei(wei_balance, "ether")
-
-    # Return the value in ether
-    return ether
-
-def send_transaction(w3, account, to, wage):
-    """Send an authorized transaction to the Ganache blockchain."""
-    # Set gas price strategy
-    w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
-
-    # Convert eth amount to Wei
-    value = w3.toWei(wage, "ether")
-
-    # Calculate gas estimate
-    gasEstimate = w3.eth.estimateGas(
-        {"to": to, "from": account.address, "value": value}
-    )
-
-    # Construct a raw transaction
-    raw_tx = {
-        "to": to,
-        "from": account.address,
-        "value": value,
-        "gas": gasEstimate,
-        "gasPrice": 0,
-        "nonce": w3.eth.getTransactionCount(account.address),
-    }
-
-    # Sign the raw transaction with ethereum account
-    signed_tx = account.signTransaction(raw_tx)
-
-    # Send the signed transactions
-    return w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    for number in range(len(people)):
+        st.image(db_list[number][4], width=200)
+        st.write("Name: ", db_list[number][0])
+        st.write("Ethereum Account Address: ", db_list[number][1])
+        st.write("KryptoJobs2Go Rating: ", db_list[number][2])
+        st.write("Hourly Rate per Ether: ", db_list[number][3], "eth")
+        st.text(" \n")
 
 # Streamlit Code
 st.markdown("# KryptoJobs2Go!")
@@ -106,9 +98,10 @@ wage = hourly_rate * hours
 st.sidebar.markdown("## Total Wage in Ether")
 st.sidebar.write(wage)
 
+# Call the send_transaction function and pass it 3 parameters: Your account, the candidate_address, and the wage as parameters
 if st.sidebar.button("Send Transaction"):
     # Send transaction and get transaction hash
-    transaction_hash = send_transaction(account, candidate_address, wage)
+    transaction_hash = send_transaction(w3, account, candidate_address, wage)
 
     # Display transaction hash
     st.sidebar.markdown("#### Validated Transaction Hash")
